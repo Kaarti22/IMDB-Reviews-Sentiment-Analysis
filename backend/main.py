7d9d9317bf0db3d57c67dda70872b3d029c9fff6
Kaarti22 <kaartimondi@gmail.com>
2025-03-07 12:13:10 +0530

    setting up FastAPI backend

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from tensorflow.keras.models import load_model
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+app = FastAPI()
+
+model = load_model("model.h5")
+
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+max_len = 200
+
+class ReviewRequest(BaseModel):
+    review: str
+
+@app.post("/predict")
+def predict_sentiment(data: ReviewRequest):
+    text = data.review
+
+    sequence = tokenizer.texts_to_sequences([text])
+    padded_sequence = pad_sequences(sequence, maxlen=max_len)
+
+    prediction = model.predict(padded_sequence)[0][0]
+
+    sentiment = "Positive" if prediction > 0.5 else "Negative"
+
+    return {"sentiment": sentiment, "confidence": float(prediction)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
